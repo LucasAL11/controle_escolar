36984cdee7a48b4a2196f41328caa6d6c957e87a
LucasAL11 <lucasluna1000@gmail.com>
2023-07-01 20:42:53 -0300

    criado Model Professores

---
+++ app/Models/ProfessorModel.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class ProfessorModel extends BaseModel {
+  public static table = 'professores'
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name : string
+
+  @column()
+  public email : string
+
+  @column()
+  public registration : string  
+
+  @column()
+  public birthdate : string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
