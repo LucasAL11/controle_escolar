65401a50d85cc7160c981285876f14aedacb5733
LucasAL11 <lucasluna1000@gmail.com>
2023-07-01 20:44:26 -0300

    [update] adicionado rota/endpoint get e delete

--- start/routes/Alunos.ts
+++ start/routes/Alunos.ts
@@ -1,4 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.get('/v1/alunos/:id', 'AlunosController.read')
 Route.post('/v1/alunos', 'AlunosController.create')
 Route.put('/v1/alunos/:id', 'AlunosController.update')
+Route.delete('/v1/alunos/:id', 'AlunosController.delete')
