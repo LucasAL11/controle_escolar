e14649b105136fa8276119858f6cf7b8781802fc
LucasAL11 <lucasluna1000@gmail.com>
2023-07-01 23:18:45 -0300

    criado controller e model de sala

---
+++ app/Controllers/Http/SalasController.ts
@@ -0,0 +1,11 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import usuario from 'App/Models/usuarios'
+import Sala from 'App/Models/SalaAula'
+
+
+export default class SalasController {
+    public async create({request}: HttpContextContract){
+
+
+    }
+}
