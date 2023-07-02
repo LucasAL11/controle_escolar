c5af1e9caf5dc26d4be6468768a4de75333329e3
LucasAL11 <lucasluna1000@gmail.com>
2023-07-02 20:06:26 -0300

    [update] melhoria no codigo em contexto geral

--- start/routes/Professores.ts
+++
@@ -1,6 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.get('/v1/professores/:id', 'professoresController.read')
-Route.post('/v1/professores', 'professoresController.create')
-Route.put('/v1/professores/:id', 'professoresController.update')
-Route.delete('/v1/professores/:id', 'professoresController.delete')
